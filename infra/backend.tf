d993e85bc3b83a49ef1e9ba78ee0285b87ce5799
Zach Adebowale <8hj.630@gmail.com>
2026-06-23 01:07:33 +0100

    feat: add Terraform backend for s3

---
+++ infra/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "airtrail-ecs-fargate-tfstate"
+    key = "dev/terraform.tfstate"
+    region = "eu-west-2"
+    encrypt = true
+    use_lockfile = true
+  }
+}
