770395ff142dbed0167dc3f847c01634263d6f5c
Zach Adebowale <8hj.630@gmail.com>
2026-06-28 00:47:56 +0100

    feat(terraform): add ACM and route53 build

--- infra/main.tf
+++ infra/main.tf
@@ -349,3 +349,43 @@ resource "aws_iam_role_policy" "github_actions" {
     ]
   })     
 }
+
+# ACM config
+
+resource "aws_acm_certificate" "main" {
+  domain_name       = "airtrail.adebowale.co.uk"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Environment = "airtrail-acm-certificate"
+  }
+}
+
+resource "aws_route53_record" "acm_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 60
+  type    = each.value.type
+  zone_id = aws_route53_zone.main.id
+}
+
+resource "aws_acm_certificate_validation" "main" {
+  certificate_arn = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
+}
+
+resource "aws_route53_zone" "main" {
+  name = "airtrail.adebowale.co.uk"
+}
