d9709ab170fbd0962f4d62a7bc71ac61d884e7b8
Zach Adebowale <8hj.630@gmail.com>
2026-06-29 23:19:55 +0100

    feat(terraform): add ecs cluster, task definition, and fargate service

--- infra/main.tf
+++ infra/main.tf
@@ -453,10 +453,10 @@ resource "aws_lb_target_group" "alb_tg" {
 
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.airtrail_alb.arn
-  port = 443
-  protocol = "HTTPS"
-  ssl_policy = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn = aws_acm_certificate_validation.main.certificate_arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = aws_acm_certificate_validation.main.certificate_arn
 
   default_action {
     type = "forward"
@@ -471,16 +471,117 @@ resource "aws_lb_listener" "https" {
 
 resource "aws_lb_listener" "http_redirect" {
   load_balancer_arn = aws_lb.airtrail_alb.arn
-  port = 80
-  protocol = "HTTP"
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
     type = "redirect"
 
     redirect {
-      port = "443"
-      protocol = "HTTPS"
+      port        = "443"
+      protocol    = "HTTPS"
       status_code = "HTTP_301"
     }
   }
+}
+
+# ECS setup
+
+resource "aws_ecs_cluster" "airtrail_ecs_cluster" {
+  name = "airtrail-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "airtrail_cloudwatch" {
+  name              = "/ecs/airtrail"
+  retention_in_days = 7
+}
+
+resource "aws_ecs_task_definition" "airtrail_task_definition" {
+  family                   = "airtrail"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "1024"
+  memory                   = "2048"
+  execution_role_arn       = aws_iam_role.ecs_execution.arn
+  task_role_arn            = aws_iam_role.ecs_execution.arn
+  container_definitions    = jsonencode([
+    {
+      name  = "airtrail"
+      image = "${aws_ecr_repository.airtrail.repository_url}:latest"
+    
+      portMappings = [
+        { 
+          containerPort = 3000
+          protocol      = "tcp"
+        }
+      ]
+
+      environment = [ 
+        {
+          name  = "ORIGIN"
+          value = "https://airtrail.adebowale.co.uk"
+        },
+        {
+          name  = "DB_URL"
+          value = "postgres://${var.db_username}:${var.db_password}@${aws_db_instance.db_instance.endpoint}/airtrail?sslmode=no-verify"
+        },
+        {
+          name  = "UPLOAD_LOCATION"
+          value = "/app/uploads"
+        }
+      ]
+  
+      health_check = {
+        command     = ["CMD-SHELL", "curl -f http://localhost:3000/api/ping || exit 1"]
+        interval    = 30 
+        timeout     = 5
+        startPeriod = 60
+        retries     = 3
+      }
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/airtrail"
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_service" "airtrail_ecs_service" {
+  name            = "airtrail-service"
+  cluster         = aws_ecs_cluster.airtrail_ecs_cluster.id
+  task_definition = aws_ecs_task_definition.airtrail_task_definition.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
+    security_groups  = [aws_security_group.ecs_sg.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+    container_name   = "airtrail"
+    container_port   = 3000
+  }
+
+  deployment_circuit_breaker {
+    enable   = true
+    rollback = true
+  }
+
+  depends_on = [
+    aws_lb_listener.https,
+    aws_iam_role_policy_attachment.ecs_execution
+   ]
 }
