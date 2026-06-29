7c78b6cd7cbf11900ba41e0dde0f0851b345312f
Zach Adebowale <8hj.630@gmail.com>
2026-06-30 00:54:41 +0100

    fix: change terraform acm and route53 resource names

--- infra/main.tf
+++ infra/main.tf
@@ -355,7 +355,7 @@ resource "aws_iam_role_policy" "github_actions" {
 
 # ACM config
 
-resource "aws_acm_certificate" "main" {
+resource "aws_acm_certificate" "airtrail_acm" {
   domain_name       = "airtrail.adebowale.co.uk"
   validation_method = "DNS"
 
@@ -370,7 +370,7 @@ resource "aws_acm_certificate" "main" {
 
 resource "aws_route53_record" "acm_validation" {
   for_each = {
-    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.airtrail_acm.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
@@ -381,15 +381,15 @@ resource "aws_route53_record" "acm_validation" {
   records = [each.value.record]
   ttl     = 60
   type    = each.value.type
-  zone_id = aws_route53_zone.main.id
+  zone_id = data.aws_route53_zone.airtrail_route53_zone.id
 }
 
-resource "aws_acm_certificate_validation" "main" {
-  certificate_arn = aws_acm_certificate.main.arn
+resource "aws_acm_certificate_validation" "acm_cert_validation" {
+  certificate_arn = aws_acm_certificate.airtrail_acm.arn
   validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
 }
 
-resource "aws_route53_zone" "main" {
+data "aws_route53_zone" "airtrail_route53_zone" {
   name = "airtrail.adebowale.co.uk"
 }
 
@@ -456,7 +456,7 @@ resource "aws_lb_listener" "https" {
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn   = aws_acm_certificate_validation.main.certificate_arn
+  certificate_arn   = aws_acm_certificate_validation.acm_cert_validation.certificate_arn
 
   default_action {
     type = "forward"
