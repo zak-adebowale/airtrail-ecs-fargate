e0b33a58d48a0c43dcdb5b96b34dfe1bf37e7125
Zach Adebowale <8hj.630@gmail.com>
2026-07-01 00:37:09 +0100

    style: change backend file format

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "airtrail-ecs-fargate-tfstate"
-    key = "dev/terraform.tfstate"
-    region = "eu-west-2"
-    encrypt = true
+    bucket       = "airtrail-ecs-fargate-tfstate"
+    key          = "dev/terraform.tfstate"
+    region       = "eu-west-2"
+    encrypt      = true
     use_lockfile = true
   }
 }
