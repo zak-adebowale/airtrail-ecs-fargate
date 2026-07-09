325e9d67ec8bed997a6bc9880ec48a1711029313
Zach Adebowale <8hj.630@gmail.com>
2026-07-09 03:54:34 +0100

    feat: remove key in backend to allow multi stage env

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
     bucket       = "airtrail-ecs-fargate-tfstate"
-    key          = "dev/terraform.tfstate"
     region       = "eu-west-2"
     encrypt      = true
     use_lockfile = true
