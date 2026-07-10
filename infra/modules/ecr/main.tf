0cdd9f5d664dd8ff1a820b53faff5c23dc58a945
Zach Adebowale <8hj.630@gmail.com>
2026-07-10 01:13:05 +0100

    chore: add force delete to tf ecr resource

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "airtrail" {
   name                 = "${var.app_name}-${var.environment}"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
