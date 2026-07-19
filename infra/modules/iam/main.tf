06b937a03e4709c9eb9f5f656ea177b8be892205
Zach Adebowale <8hj.630@gmail.com>
2026-07-19 15:54:32 +0100

    style: format tf files (github iam resource)

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -32,7 +32,7 @@ resource "aws_iam_role_policy_attachment" "ecs_execution" {
 }
 
 resource "aws_iam_role" "github_actions" {
-  name = "${var.app_name}-${var.environment}-github-actions-role"
+  name                 = "${var.app_name}-${var.environment}-github-actions-role"
   max_session_duration = 14400
 
   assume_role_policy = jsonencode({
