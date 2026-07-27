ba778c351ead7c04aeb69ab338771e83494a6511
Zach Adebowale <8hj.630@gmail.com>
2026-07-27 02:24:01 +0100

    feat(terraform): replace deprecated security group arguments with ingress/egress resources

--- infra/modules/security_groups/main.tf
+++ infra/modules/security_groups/main.tf
@@ -14,33 +14,37 @@ resource "aws_security_group" "alb_sg" {
   description = "Allow inbound HTTP and HTTPS from internet"
   vpc_id      = var.vpc_id
 
-  ingress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = [var.all_ip_cidr]
-  }
-
-  ingress {
-    description = "Allow HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [var.all_ip_cidr]
-  }
-
-  egress {
-    description = "Allow outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [var.all_ip_cidr]
-  }
-
   tags = {
     Name = "${var.app_name}-${var.environment}-alb-sg"
   }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_allow_http" {
+  security_group_id = aws_security_group.alb_sg.id
+
+  description = "Allow HTTP"
+  cidr_ipv4   = var.all_ip_cidr
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_allow_https" {
+  security_group_id = aws_security_group.alb_sg.id
+
+  description = "Allow HTTPS"
+  cidr_ipv4   = var.all_ip_cidr
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_allow_all_outbound" {
+  security_group_id = aws_security_group.alb_sg.id
+
+  description = "Allow outbound traffic"
+  cidr_ipv4   = var.all_ip_cidr
+  ip_protocol = "-1"
 }
 
 resource "aws_security_group" "ecs_sg" {
@@ -48,24 +52,27 @@ resource "aws_security_group" "ecs_sg" {
   description = "Allow inbound from ALB"
   vpc_id      = var.vpc_id
 
-  ingress {
-    description     = "Allow traffic from ALB"
-    from_port       = 3000
-    to_port         = 3000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
-
-  egress {
-    description = "Allow outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [var.all_ip_cidr]
-  }
   tags = {
     Name = "${var.app_name}-${var.environment}-ecs-sg"
   }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ecs_allow_alb_traffic" {
+  security_group_id            = aws_security_group.ecs_sg.id
+  referenced_security_group_id = aws_security_group.alb_sg.id
+
+  description = "Allow traffic from ALB"
+  from_port   = 3000
+  ip_protocol = "tcp"
+  to_port     = 3000
+}
+
+resource "aws_vpc_security_group_egress_rule" "ecs_allow_all_outbound" {
+  security_group_id = aws_security_group.ecs_sg.id
+
+  description = "Allow outbound traffic"
+  cidr_ipv4   = var.all_ip_cidr
+  ip_protocol = "-1"
 }
 
 resource "aws_security_group" "rds_sg" {
@@ -73,23 +80,25 @@ resource "aws_security_group" "rds_sg" {
   description = "Allow inbound PostgreSQL from ECS"
   vpc_id      = var.vpc_id
 
-  ingress {
-    description     = "Allow PostgreSQL from ECS"
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ecs_sg.id]
-  }
-
-  egress {
-    description = "Allow outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = [var.all_ip_cidr]
-  }
-
   tags = {
     Name = "${var.app_name}-${var.environment}-rds-sg"
   }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "rds_allow_postgre_from_ecs" {
+  security_group_id            = aws_security_group.rds_sg.id
+  referenced_security_group_id = aws_security_group.ecs_sg.id
+
+  description = "Allow PostgreSQL from ECS"
+  from_port   = 5432
+  ip_protocol = "tcp"
+  to_port     = 5432
+}
+
+resource "aws_vpc_security_group_egress_rule" "rds_allow_all_outbound" {
+  security_group_id = aws_security_group.rds_sg.id
+
+  description = "Allow outbound traffic"
+  cidr_ipv4   = var.all_ip_cidr
+  ip_protocol = "-1"
 }
